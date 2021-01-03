87c268c60330113fb66f6fc281d0ae0f2081f85c
Steve Mansour <sman@stevemansour.com>
2021-01-03 13:53:39 -0800

    UPDATE: phototable supports click to full-size-view

--- webui/js/elems/pics.js
+++ webui/js/elems/pics.js
@@ -1,6 +1,6 @@
 /*global
     w2ui, app, $, console, dateFmtStr, propData, Promise, document, FormData,
-    fetch,w2confirm, doDeletePhoto,
+    fetch,w2confirm, doDeletePhoto,window,
 */
 
 "use strict";
@@ -74,13 +74,38 @@ function AwaitImagePanelRenderComplete() {
     }
 }
 
+// // propChangeImage allows the user to change the selected image.
+// //
+// // INPUTS
+// // x = the index number 1 - 8 for which image they're switching
+// //
+// //-----------------------------------------------------------------------
+// function propChangeImage(x) {
+//     var f = document.getElementById("FileUpload"+x);
+//     if (f == null) {
+//         return;
+//     }
+//     var fp = document.getElementById("spnFilePath" + x);
+//     if (fp == null ) {
+//         return;
+//     }
+//     var image = "Img" + x;
+//     var idx = x;
+//     f.onchange = function() {
+//         var fileName = f.value.split('\\')[f.value.split('\\').length - 1];
+//         fp.innerHTML = "uploading " + fileName;
+//         w2ui.propertyForm.record[image] = fileName;
+//         SavePhotoToServer(fileName,idx,f.files[0]);
+//     }
+// }
+
 function SetUpImageCatchers() {
     var fileupload1 = document.getElementById("FileUpload1");
     var filePath1 = document.getElementById("spnFilePath1");
-    var image = document.getElementById("phototable1");
-    image.onclick = function() {
-        fileupload1.click();
-    };
+    var pt1 = document.getElementById("phototable1");
+    var image1 = document.getElementById("editPic1");
+    image1.onclick = function() { fileupload1.click(); };
+    pt1.onclick = function() { fileupload1.click(); };
     fileupload1.onchange = function() {
         var fileName = fileupload1.value.split('\\')[fileupload1.value.split('\\').length - 1];
         filePath1.innerHTML = "uploading " + fileName;
@@ -90,10 +115,10 @@ function SetUpImageCatchers() {
 
     var fileupload2 = document.getElementById("FileUpload2");
     var filePath2 = document.getElementById("spnFilePath2");
-    var image2 = document.getElementById("phototable2");
-    image2.onclick = function() {
-        fileupload2.click();
-    };
+    var pt2 = document.getElementById("phototable2");
+    var image2 = document.getElementById("editPic2");
+    image2.onclick = function() { fileupload2.click(); };
+    pt2.onclick = function() { fileupload2.click(); };
     fileupload2.onchange = function() {
         var fileName2 = fileupload2.value.split('\\')[fileupload2.value.split('\\').length - 1];
         filePath2.innerHTML = "uploading " + fileName2;
@@ -103,10 +128,10 @@ function SetUpImageCatchers() {
 
     var fileupload3 = document.getElementById("FileUpload3");
     var filePath3 = document.getElementById("spnFilePath3");
-    var image3 = document.getElementById("phototable3");
-    image3.onclick = function() {
-        fileupload3.click();
-    };
+    var pt3 = document.getElementById("phototable3");
+    var image3 = document.getElementById("editPic3");
+    image3.onclick = function() { fileupload3.click(); };
+    pt3.onclick = function() { fileupload3.click(); };
     fileupload3.onchange = function() {
         var fileName3 = fileupload3.value.split('\\')[fileupload3.value.split('\\').length - 1];
         filePath3.innerHTML = "uploading " + fileName3;
@@ -116,10 +141,10 @@ function SetUpImageCatchers() {
 
     var fileupload4 = document.getElementById("FileUpload4");
     var filePath4 = document.getElementById("spnFilePath4");
-    var image4 = document.getElementById("phototable4");
-    image4.onclick = function() {
-        fileupload4.click();
-    };
+    var pt4 = document.getElementById("phototable4");
+    var image4 = document.getElementById("editPic4");
+    image4.onclick = function() { fileupload4.click(); };
+    pt4.onclick = function() { fileupload4.click(); };
     fileupload4.onchange = function() {
         var fileName4 = fileupload4.value.split('\\')[fileupload4.value.split('\\').length - 1];
         filePath4.innerHTML = "uploading " + fileName4;
@@ -129,10 +154,10 @@ function SetUpImageCatchers() {
 
     var fileupload5 = document.getElementById("FileUpload5");
     var filePath5 = document.getElementById("spnFilePath5");
-    var image5 = document.getElementById("phototable5");
-    image5.onclick = function() {
-        fileupload5.click();
-    };
+    var pt5 = document.getElementById("phototable5");
+    var image5 = document.getElementById("editPic5");
+    image5.onclick = function() { fileupload5.click(); };
+    pt5.onclick = function() { fileupload5.click(); };
     fileupload5.onchange = function() {
         var fileName5 = fileupload5.value.split('\\')[fileupload5.value.split('\\').length - 1];
         filePath5.innerHTML = "uploading " + fileName5;
@@ -142,10 +167,10 @@ function SetUpImageCatchers() {
 
     var fileupload6 = document.getElementById("FileUpload6");
     var filePath6 = document.getElementById("spnFilePath6");
-    var image6 = document.getElementById("phototable6");
-    image6.onclick = function() {
-        fileupload6.click();
-    };
+    var pt6 = document.getElementById("phototable6");
+    var image6 = document.getElementById("editPic6");
+    image6.onclick = function() { fileupload6.click(); };
+    pt6.onclick = function() { fileupload6.click(); };
     fileupload6.onchange = function() {
         var fileName6 = fileupload6.value.split('\\')[fileupload6.value.split('\\').length - 1];
         filePath6.innerHTML = "uploading " + fileName6;
@@ -155,10 +180,10 @@ function SetUpImageCatchers() {
 
     var fileupload7 = document.getElementById("FileUpload7");
     var filePath7 = document.getElementById("spnFilePath7");
-    var image7 = document.getElementById("phototable7");
-    image7.onclick = function() {
-        fileupload7.click();
-    };
+    var pt7 = document.getElementById("phototable7");
+    var image7 = document.getElementById("editPic7");
+    image7.onclick = function() { fileupload7.click(); };
+    pt7.onclick = function() { fileupload7.click(); };
     fileupload7.onchange = function() {
         var fileName7 = fileupload7.value.split('\\')[fileupload7.value.split('\\').length - 1];
         filePath7.innerHTML = "uploading " + fileName7;
@@ -168,10 +193,10 @@ function SetUpImageCatchers() {
 
     var fileupload8 = document.getElementById("FileUpload8");
     var filePath8 = document.getElementById("spnFilePath8");
-    var image8 = document.getElementById("phototable8");
-    image8.onclick = function() {
-        fileupload8.click();
-    };
+    var pt8 = document.getElementById("phototable8");
+    var image8 = document.getElementById("editPic8");
+    image8.onclick = function() { fileupload8.click(); };
+    pt8.onclick = function() { fileupload8.click(); };
     fileupload8.onchange = function() {
         var fileName8 = fileupload8.value.split('\\')[fileupload8.value.split('\\').length - 1];
         filePath8.innerHTML = "uploading " + fileName8;
@@ -259,12 +284,27 @@ function deleteImg(x) {
 
     w2confirm('Are you sure you want to delete image '+x)
     .yes(function () {
-        doDeletePhoto(x);
-    })
+            doDeletePhoto(x);
+        }
+    )
     .no(function () {
-        console.log('NO');
-     }
- );
+            console.log('NO');
+         }
+    );
+}
+
+// propImageView loads the full image into a new page.
+//
+// INPUTS
+//     x = index number of the image.
+//------------------------------------------------------------------------------
+function propImageView(x) {
+    var id='Img' + x;
+    var url=w2ui.propertyForm.record[id];
+    if (url.length < 1) {
+        return;
+    }
+    window.open(url);
 }
 
 // doDeletePhoto is called when the user acknowledges that they want to delete
